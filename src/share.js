31d94c3e7754cbc198b7a0c47aa7a70432360325
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-06-29 17:43:36 +0800

    Add type props

--- src/share.js
+++ src/share.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Utils from './utils';
+import React from "react";
+import PropTypes from "prop-types";
+import Utils from "./utils";
 
 class Share extends React.Component {
   componentDidMount() {
@@ -12,16 +12,17 @@ class Share extends React.Component {
     Utils.loadButtons();
   }
   render() {
-    const { style, className, locale } = this.props;
-    const url = Utils.fixUrl( this.props.url );
+    const { style, className, locale, type } = this.props;
+    const url = Utils.fixUrl(this.props.url);
 
     return (
       <div
-        style={ Object.assign( {}, { display: 'none' }, style ) }
-        className={ 'line-it-button' + ( className ? '' : ' ' + className ) }
-        data-lang={ Utils.localeToLang( locale ) }
-        data-type="share-a"
-        data-url={ url } />
+        style={Object.assign({}, { display: "none" }, style)}
+        className={"line-it-button" + (className ? "" : " " + className)}
+        data-lang={Utils.localeToLang(locale)}
+        data-type={"share-" + type}
+        data-url={url}
+      />
     );
   }
 }
@@ -29,12 +30,14 @@ class Share extends React.Component {
 Share.propTypes = {
   style: PropTypes.object,
   className: PropTypes.string,
-  locale: PropTypes.oneOf( [ 'en', 'ja', 'zh-TW', 'th', 'id' ] ),
+  locale: PropTypes.oneOf(["en", "ja", "zh-TW", "th", "id"]),
+  type: PropTypes.oneOf(["a", "b", "c", "d", "e"]),
   url: PropTypes.string
 };
 
 Share.defaultProps = {
-  locale: 'zh-TW'
+  locale: "zh-TW",
+  type: "c"
 };
 
 export default Share;
